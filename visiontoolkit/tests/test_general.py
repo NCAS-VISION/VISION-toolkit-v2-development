64deee554aa8d4dc9357059d3181deb9e478d200
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-02-28 17:49:04 +0000

    Add first test method, test_help for CLI help only

--- visiontoolkit/tests/test_general.py
+++ visiontoolkit/tests/test_general.py
@@ -16,12 +16,33 @@ class TestGeneral:
             with pytest.raises(SystemExit) as excinfo:
                 visiontoolkit.main()  # Run command
                 
-        captured = capsys.readouterr().out
+        cmd_stdout = capsys.readouterr().out
+        cmd_stdout_ignore_newlines = cmd_stdout.replace("\n", " ")
 
         # Actual testing statements
-        assert captured != ""
-        assert "usage: VISION TOOLKIT" in captured
-        
+        # 1. Assert something emerges to STDOUT
+        assert cmd_stdout != ""
+
+        # 2. Assert basic text including summary appears
+        assert "usage: VISION TOOLKIT" in cmd_stdout
+        assert (
+            "Virtual Integration of Satellite and In-Situ Observation "
+            "Networks (VISION) toolkit flight simulator"
+        ) in cmd_stdout_ignore_newlines
+
+        # 3. Assert ASCII project symbol appears
+        # Just test this via first line to avoid having to wrestle against
+        # newlines and whitespace line splitting
+        assert ".______________________________________________." in cmd_stdout
+
+        # 4. Assert some CLI argument help descriptions appear.
+        # Don't want to test on any specific option since thes might change
+        # with a CLI change, but this 'help' command is guaranteed to be there
+        # so check the description of that appears.
+        assert "options:" in cmd_stdout
+        assert (
+            "-h, --help            show this help message and exit"
+        ) in cmd_stdout_ignore_newlines
 
 
 if __name__ == "__main__":
