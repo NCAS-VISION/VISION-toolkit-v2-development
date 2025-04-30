104442de4ec3acbf1a95ed422806f44dcf15851c
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-04-30 15:59:21 +0100

    Add module missing-to-repo: plotting/visualisation

---
+++ visiontoolkit/plotting.py
@@ -0,0 +1,157 @@
+"""Plotting module of the VISION toolkit. All plotting functionality is here."""
+
+import logging
+
+# NOTE: keep this order (cfp then cf imported) to avoid Seg Fault issues
+import cfplot as cfp
+import cf
+
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+def preview_plots(
+    obs_field,
+    show_plot_of_input_obs,
+    plot_of_input_obs_track_only,
+    outputs_dir,
+    plotname_start,
+    cfp_mapset_config,
+    cfp_cscale,
+    cfp_input_levs_config,
+    cfp_input_track_only_config,
+    cfp_input_general_config,
+    verbose,
+    index=False,
+):
+    """Generate plots of the flight track for a pre-colocation preview.
+
+    If index is provided, it is assumed there will be multiple preview plots
+    and therefore each should be labelled with the index in the name.
+
+    TODO: DETAILED DOCS
+    """
+    # First configure general settings for plot
+    # Change the viewpoint to be over the UK only, with high-res map outline
+    cfp.mapset(**cfp_mapset_config)
+    cfp.cscale(cfp_cscale)
+    if index is False:  # distinguish it from 0, a possible index
+        index = ""  # so the name does not change
+    else:
+        index = f"_{index}"
+
+    if show_plot_of_input_obs:
+        # Plot *input* observational data for a preview, before doing anything
+        # Min, max as determined using final_result_field.min(), .max():
+        if cfp_input_levs_config:
+            cfp.levs(**cfp_input_levs_config)
+        if plot_of_input_obs_track_only in (1, 2):
+            # Use the same field but set all data to zero so can plot the whole
+            # track in the same colour to just display the path, not orig. data
+            equal_data_obs_field = obs_field.copy()
+            new_data = np.zeros(
+                len(equal_data_obs_field.data)
+            )  # 0 -> force red with colour scheme set later
+            equal_data_obs_field.set_data(new_data, inplace=True)
+
+            # Not configurable, always use since it gives red for zero values
+            # therefore whole track will be red to make it clear it is a block
+            # colour without meaning attached
+            cfp.cscale("scale28")
+            cfp.gopen(
+                file=(
+                    f"{outputs_dir}/"
+                    f"{plotname_start}_obs_track_only{index}.png"
+                )
+            )
+            cfp_input_track_only_config.update(verbose=verbose)
+            cfp.traj(
+                equal_data_obs_field, **cfp_input_track_only_config
+            )
+            cfp.gclose()
+            cfp.cscale(
+                cfp_cscale
+            )  # reset for normal (default-style) plots after
+        if plot_of_input_obs_track_only in (0, 2):
+            cfp.gopen(
+                file=(
+                    f"{outputs_dir}/"
+                    f"{plotname_start}_obs_track_with_data_{index}.png"
+                )
+            )
+            cfp_input_general_config.update(verbose=verbose)
+            cfp.traj(obs_field, **cfp_input_general_config)
+            cfp.gclose()
+
+
+def output_plots(
+    output,
+    obs_t_identifier,
+    cfp_output_levs_config,
+    outputs_dir,
+    plotname_start,
+    new_obs_starttime,
+    cfp_output_general_config,
+    verbose,
+    preprocess_model=False,
+):
+    """Generate a post-colocation result plot of the track(s) or swath(s).
+
+    The plot may optionally be displayed during script execution, else
+    saved to disk.
+
+    TODO: DETAILED DOCS
+    """
+    cfp_output_general_config.update(verbose=verbose)
+
+    # Upgrade the aux coor to a dim coor, so we can plot the trajectory.
+    # TODO: avoid doing this, as is not 'proper', when there is a way to
+    #       just use the aux. coor for cfp.traj: the way to support in a new
+    #       cf-plot version is to check if the input is a featureType, then
+    #       if it is to look for aux coords not dim coords, since if it is one
+    #       there should never be dim coords.
+    # TODO: another cpflot feature that will help here: generalise the
+    #       trajectory function for not just contiguous ragged array, as
+    #       the present docs state, but for any *multidimensional orthogonal
+    #       arrays* e.g. DSGs, as here. Talking about 'ragged arrays' is a
+    #       massive red herring. In which case, generalise it so that the input
+    #       can be a field with a 2D *or* a 1D array to plot. If 1D, it means
+    #       it has a trajectory dimension leading, which can be dropped.
+
+    # WRF ONLY, TODO move underlying logic to pre-processing so as not to
+    # clog up main module
+    if preprocess_model == "WRF":
+        aux_coor_t = output.auxiliary_coordinate(obs_t_identifier)
+        dim_coor_t = cf.DimensionCoordinate(source=aux_coor_t)
+        output.set_construct(dim_coor_t, axes="ncdim%obs")
+
+    # Make and open the final plot
+    # NOTE: can try 'legend_lines=True' for the lines plotted with average
+    #       between the two scatter marker points, if preferable?
+    cfp.gopen(
+        file=f"{outputs_dir}/{plotname_start}_final_colocated_field.png",
+    )
+
+    # Set levels for plotting of data in a colourmap
+    # Min, max as determined using output.min(), .max():
+    if cfp_output_levs_config:
+        cfp.levs(**cfp_output_levs_config)
+
+    # Note the set start time of the obs on the plot title as key info.
+    if new_obs_starttime:
+        update_title = f"assuming starting time of {new_obs_starttime}"
+        orig_title = cfp_output_general_config.get("title", None)
+        if orig_title:
+            cfp_output_general_config.update(
+                title=f"{orig_title} {update_title}"
+            )
+        else:
+            cfp_output_general_config["title"] = update_title.title()
+
+    cfp.traj(output, **cfp_output_general_config)
+
+    cfp.gclose()
+    logger.info("Plot created.")
+
